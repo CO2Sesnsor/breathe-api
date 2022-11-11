ff5ebdc2cecfe473adc4adee776cebe54f36aeb8
Hasin Mahmood <hasinmahmood01@gmail.com>
2022-11-11 12:39:46 +0000

    send response instead of render

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get("/", function (req, res, next) {
-  res.render("index", { title: "Breathe" });
+  res.send("index", { title: "Breathe" });
 });
 
 module.exports = router;
